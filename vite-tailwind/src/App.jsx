609c8d3d73eefefb062ba2483527cdd40c9c30b6
Nitin Dhyani <nitindhyani1234@gmail.com>
2025-05-13 22:53:02 +0530

    day 4

---
+++ vite-tailwind/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1 className='bg-green-400 p-4 text-black rounded'>Tailwind Test</h1>
+    </>
+  )
+}
+export default App
