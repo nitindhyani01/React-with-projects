18595220d5a8b739404652e39876473f58221bd0
Nitin Dhyani <nitindhyani1234@gmail.com>
2025-05-14 21:32:25 +0530

    day 5

--- vite-tailwind/src/App.jsx
+++ vite-tailwind/src/App.jsx
@@ -2,11 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './card.jsx'
 
 function App() {
+  let myObj={
+  username:"Nitin",
+  age: 21
+}
   return (
     <>
-     <h1 className='bg-green-400 p-4 text-black rounded'>Tailwind Test</h1>
+     <h1 className='bg-green-400 p-4 text-black rounded mb-3'>Tailwind Test</h1>
+     <Card username="Nitin" btntext='Click Me'/>
+     <Card username="Aman" btntext='Click Me'/>
     </>
   )
 }
